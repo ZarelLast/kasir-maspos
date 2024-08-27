5edb301bc2838362772783dfbb927ec8291f9ee9
Muhammad Ilham Triwibowo <triwibowoilham2@gmail.com>
2024-08-27 23:47:08 +0700

    +add button primary-outline

---
+++ src/components/Button/PrimaryOutline.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="border-blue-400 p2 rouded-md text-blue-400 hover:border-blue-500 hover:text-blue-500" :type="type">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'button'
+    }
+  }
+}
+</script>
